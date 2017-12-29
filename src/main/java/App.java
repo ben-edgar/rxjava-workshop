b9e98e1f9ad93c0f200ba0af9929570a5629793f
Ben Edgar <ben.edgar@appian.com>
2017-12-29 12:32:01 -0500

    Initial version of a command line app

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,39 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Scanner;
+
 import io.reactivex.Observable;
 
 public class App {
 
-  public static void main(String[] args) {
-    Observable.range(1, 5).subscribe(
-        number -> System.out.println("Your current balance is: " + number),
-        error -> System.out.println("Error: " + error),
-        () -> System.out.println("Done")
+  public static void main(String[] args) throws InterruptedException {
+    Queue<Integer> myQueue = new PriorityQueue<>();
+
+    myQueue.add(1);
+
+    Observable<Integer> integerObservable = Observable.fromIterable(myQueue);
+
+    integerObservable.subscribe(
+        number -> System.out.println("You entered this number: " + number),
+        error -> System.out.println("There was an error!")
     );
+
+    Scanner scanner = new Scanner(System.in);
+
+    while (true) {
+      Thread.sleep(1 * 1000);
+      System.out.println("Enter a number or numbers separated by spaces: ");
+      String input = scanner.nextLine();
+      String[] integerArray = input.split(" ");
+      for (String s : integerArray) {
+        try {
+          Integer i = Integer.valueOf(s);
+          myQueue.add(i);
+
+        } catch (NumberFormatException e) {
+          integerObservable.onTerminateDetach();
+        }
+      }
+    }
   }
 }
